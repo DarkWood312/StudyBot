16dbb7f7f64e449dae7f7ea3a0d07c56a0a6cffb
Aleksandr <sasha.gabrielyan.2005@mail.ru>
2022-11-12 23:53:06 +0300

    first commit

---
+++ gdz.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+from aiogram import types
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 '
+                  'Safari/537.36 '
+}
+
+
+async def process(arr: list, sep: int = 10):
+    l = [types.InputMediaPhoto(i) for i in arr]
+    return [l[i:i + sep] for i in range(0, len(l), sep)]
+
+class GDZ:
+
+
+    async def algru(num: int):
+        imgs = []
+        r = requests.get(f'https://gdz.ru/class-10/algebra/alimov-15/{num}-nom/', headers=headers)
+        raw_imgs = BeautifulSoup(r.content, 'lxml').find_all(class_='with-overtask')
+        for img in raw_imgs:
+            imgs.append('https:' + img.find('img')['src'])
+        return await process(imgs)
---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from gdz import GDZ
+import db
+
+API_TOKEN = '5681353642:AAHQ-tjhwjU0yX7rrZWS2BHqXWhrcAS4jww'
+logging.basicConfig(level=logging.DEBUG)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.answer(db.gdz_help, parse_mode=types.ParseMode.MARKDOWN)
+
+
+@dp.message_handler()
+async def other_messages(message: types.Message):
+    low = message.text.lower()
+    # *  gdz...
+    try:
+        if ('алг ' in low) or ('alg' in low):
+            num = int(low.split(' ')[1])
+            for mediagroup in await GDZ.algru(num):
+                await message.answer_media_group(mediagroup)
+    except ValueError:
+        await message.answer('Неправильно введен номер задания')
+        await message.answer(db.gdz_help, parse_mode=types.ParseMode.MARKDOWN)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
