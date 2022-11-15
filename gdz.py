d88e53f744ec3b8243ab78d7c094ac81b19e60cf
Aleksandr <sasha.gabrielyan.2005@mail.ru>
2022-11-15 17:01:14 +0300

    test

--- gdz.py
+++ gdz.py
@@ -9,7 +9,8 @@ headers = {
     'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'referer': 'https://gdz.ru/class-10/algebra/alimov-15/',
     'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-    'authority': 'gdz.ru'
+    'authority': 'gdz.ru',
+    'cookie': '_ym_uid=1663967487128278999; _ym_d=1663967487; cf_clearance=ceEqJPPMn9KJ_by3NYlmGl2F12cLM.Rv31DXluKbZhc-1668270949-0-150; _ym_isad=1; __cf_bm=aHW3nb97Sv4aGVGG54jUjNOagDwPC3bPx8zPPTO.gQg-1668520084-0-ATrHl9SmniBGAi4vuEnRS+hGGqoLdzn1G+Q+1eHfffJLcPjKizwOzqiDD7N3xWr/fv07QSgNql1N5eto9ANqO8/9TeNjolYZ7YOys7omVg+RDT/z2jvFoJpLD1nACQuCjA=='
 }
 
 
