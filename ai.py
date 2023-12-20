ac51e9037495c00534d61925165698de54eec38a
DarkWood312 <44950472+DarkWood312@users.noreply.github.com>
2023-12-20 12:31:01 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -65,7 +65,7 @@ async def image_ai_tg(message: Message, state: FSMContext, bot: Bot, ai_method, 
         await cancel(message, state)
         return
     try:
-        img = await ai_method(message.text, True)
+        img = await ai_method(message.text, ' ')
         await message.answer_photo(BufferedInputFile(img, message.text),
                                    caption=f'<b>{ai_name}🦋:</b> <code>{html.quote(message.text)}</code>\n@{(await bot.get_me()).username}')
     except ValueError as e:
