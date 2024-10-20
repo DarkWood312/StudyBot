665722f94137317a61f0f2c8f0b0b033d5c2dc45
DarkWood312 <44950472+DarkWood312@users.noreply.github.com>
2024-10-20 21:53:03 +0300

    upd

--- extra/config.py
+++ extra/config.py
@@ -9,10 +9,11 @@ load_dotenv()
 token = os.getenv('API_TOKEN')
 openai_api = os.getenv('OPENAI_API') or None
 wolfram_api = os.getenv('WOLFRAM_API') or None
-uchus_cookies = {k: v for k, v in [i.split('=', 1) for i in os.getenv('UCHUS_COOKIES').split(';')]} or None
+uchus_cookies = os.getenv('UCHUS_COOKIES') or None
 redis_host, redis_password, redis_port = os.getenv('REDIS_HOST') or None, os.getenv('REDIS_PASSWORD') or None, os.getenv('REDIS_PORT') or '5432'
 deep_translate_api = os.getenv('DEEP_TRANSLATE_API') or None
 
+uchus_cookies = {k: v for k, v in [i.split('=', 1) for i in uchus_cookies.split(';')]}
 try:
     sql = Sqdb(os.getenv('SQL_HOST'), os.getenv('SQL_PASSWORD'), os.getenv('SQL_PORT'), os.getenv('SQL_DATABASE'), os.getenv('SQL_USER'))
 except OperationalError:
